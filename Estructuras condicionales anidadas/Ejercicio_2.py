a0447522a8bb5a927a59e7a9a8672acdfd3d17e3
Ossus <gilberto-247@hotmail.com>
2021-10-22 23:11:44 -0700

    Adding exercise2

--- Estructuras condicionales anidadas/Ejercicio_2.py
+++ Estructuras condicionales anidadas/Ejercicio_2.py
@@ -1,3 +1,11 @@
 #Se ingresa por teclado un valor entero,
 #mostrar una leyenda que indique si el número es positivo, 
 #negativo o nulo (es decir cero)
+
+numero = int(input('Ingrese el valor'))
+if numero > 0:
+    print('El valor es positivo')
+elif numero < 0:
+    print('El valor es negativo')
+else:
+    print('El valor es nulo')
